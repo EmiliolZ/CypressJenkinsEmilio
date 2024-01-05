20299cfff9b6ae95c7377be6a4de88a70dabe6b3
EmiliolZ <zelarayanel@gmail.com>
2024-01-05 16:29:03 -0300

    "add falling test"
    "add falling test"

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,12 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Test de pruebas para Jenkis', () => {
+  it('Test que pasa', () => {
+    cy.visit('https://example.cypress.io')
+  })
+  it('Test que falla', () => {
+    cy.visit('https://example.cypress.io')
+    cy.get('h1').shoul("have.text", "Hola hola");
+  })
+  it('Test que no se ejecuta', () => {
     cy.visit('https://example.cypress.io')
   })
 })
